1cb87d9c303911104ebbd4c4eba9975a940ef45e
zhouwei <1583230969@qq.com>
2019-01-22 21:33:41 +0800

    demo.java

---
+++ demo.java
@@ -0,0 +1,41 @@
+package com.bean;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class demo {
+
+	public static void main(String[] args) {
+		ThreadPoolExecutor th =new ThreadPoolExecutor(2, 5, 10L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+		Bank b =new Bank();
+		for(int i=0; i<5; i++) {
+			//new Thread(b).start();
+			th.execute(b);
+		}
+		
+	}
+}
+ class Bank implements Runnable{
+	 int count =10;
+	 
+	 public synchronized void add() {
+		 count++;
+		System.out.println(Thread.currentThread().getName()+"count:"+count);
+		reduce();
+	 }
+	 
+	 public synchronized void reduce() {
+		 count--;
+	 }
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		add();
+	}
+	 
+ }
+ 
+	 
+	 
