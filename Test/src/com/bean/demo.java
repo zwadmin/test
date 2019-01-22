14dcccade7b814d715d89fa8843eab92d0656204
zwadmin <46894495+zwadmin@users.noreply.github.com>
2019-01-22 21:51:11 +0800

    Update demo.java

--- Test/src/com/bean/demo.java
+++ Test/src/com/bean/demo.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class demo {
-//22222222222222
+//fffffffffffff
 	public static void main(String[] args) {
 		ThreadPoolExecutor th =new ThreadPoolExecutor(2, 5, 10L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
 		Bank b =new Bank();
